0c426cfe774a9bbff1bed3adb26f42228656d9e6
Verma, Aryan <techcowryan@gmail.com>
2025-03-23 15:17:45 +0530

    feat: update startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 sudo mkdir -p /etc/letsencrypt/live/demo.qxli.com
 sudo mkdir -p /etc/letsencrypt/live/agents.qxli.com
+sudo mkdir -p /etc/letsencrypt/live/flow.qxli.com
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
     | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
